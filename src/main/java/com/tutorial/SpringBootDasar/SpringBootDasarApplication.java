54a7274cfb412f7206a4995b69e96ab4fe85d26d
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-05-21 15:01:11 +0700

    first commit

---
+++ src/main/java/com/tutorial/SpringBootDasar/SpringBootDasarApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.SpringBootDasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDasarApplication.class, args);
+	}
+
+}
